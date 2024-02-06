d624cf89b695daf3fa0d8fa0c618ef1098a6908b
Víctor Orozco <veom104@gmail.com>
2024-02-06 13:10:16 -0800

    ✨ [ADD] page Home
    add Home page whit Boards examples

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,44 @@
+import { BoardFinal } from "./lib/component/boards/boardFinal";
+import Board from "./lib/component/boards/boart";
 import { Header } from "./lib/component/header";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <section className="container mx-auto grid py-16 w-[80%] max-h-screen gap-8 ">
+        <div className="w-40 grid gap-4">
+          <h1>My Boards</h1>
+          <div className="grid gap-2 ">
+            <p>Sort by</p>
+            <select
+              name="sort"
+              id="sort"
+              className="text-black rounded-md p-2 h-10"
+            >
+              <option value="date" selected>
+                Date
+              </option>
+              <option value="name">Name</option>
+            </select>
+          </div>
+        </div>
+
+        {/* space for Boards */}
+        <div className="flex  justify-between gap-8 h-full  w-full flex-wrap">
+          <Board />
+          <BoardFinal />
+          <BoardFinal />
+          <BoardFinal />
+          <BoardFinal />
+          <BoardFinal />
+          <BoardFinal />
+          <BoardFinal />
+          <BoardFinal />
+          <BoardFinal />
+          <BoardFinal />
+        </div>
+      </section>
     </main>
   );
 }
