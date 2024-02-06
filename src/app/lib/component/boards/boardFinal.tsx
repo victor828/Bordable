db3d82563070bccebd391ea06b450a80970e4b96
Víctor Orozco <veom104@gmail.com>
2024-02-06 13:08:05 -0800

    ✨ [ADD] new compnent Board
    add the new component Board

---
+++ src/app/lib/component/boards/boardFinal.tsx
@@ -0,0 +1,14 @@
+import { inter } from "../../ui/fonts";
+
+export function BoardFinal() {
+  /* // TODO: 
+      crear la lista de paleta de color para los colorres especificados
+      definir el olor dependiendo del color seleccionado en el imput este viene de la bas e de datos
+      
+    */
+  return (
+    <button className={`${inter} w-64 h-36 p-4 bg-gray-300 rounded-lg`}>
+      title
+    </button>
+  );
+}
