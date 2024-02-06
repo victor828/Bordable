c2c23dccc79815cf1aee151c0a5e4f5c0291a21b
Víctor Orozco <veom104@gmail.com>
2024-02-06 05:18:21 -0800

    📝 [ADD] main page and css, global and fonts
    Agree document page and css

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import s from "./index.module.css";
+import { familjen } from "@/app/lib/ui/fonts";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+  );
+}
