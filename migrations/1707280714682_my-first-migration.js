c3d9c3257b0ba934f344b46ab1a8409c637170c6
Víctor Orozco <veom104@gmail.com>
2024-02-06 20:49:37 -0800

    📝 [ADD] migration
    agree my fist migration, this contain all tables for use in the proyect

---
+++ migrations/1707280714682_my-first-migration.js
@@ -0,0 +1,47 @@
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: { type: "serial", primaryKey: true },
+    username: { type: "varchar(50)", unique: true, notNull: true },
+    name: { type: "varchar(255)" },
+    email: { type: "varchar(255)" },
+    password: { type: "varchar(255)", notNull: true },
+    type: {
+      type: "varchar(10)",
+      default: "'admin'::varchar",
+      check: "type IN ('user', 'admin')",
+    },
+  });
+
+  pgm.createTable("Boards", {
+    id: { type: "serial", primaryKey: true },
+    title: { type: "varchar(255)", unique: true, notNull: true },
+    color: { type: "varchar(50)" },
+    userid: { type: "int" },
+    createdate: { type: "date", default: "CURRENT_DATE" },
+    updatedate: { type: "date", default: "CURRENT_DATE" },
+    userid_fk: {
+      type: "integer",
+      references: '"users"',
+      notNull: false,
+      onDelete: "CASCADE",
+    },
+  });
+
+  pgm.createTable("tables", {
+    boardid: { type: "int" },
+    title: { type: "varchar(50)", unique: true, notNull: true },
+    card: { type: "varchar(255)" },
+    boardid_fk: {
+      type: "integer",
+      references: '"Boards"',
+      notNull: false,
+      onDelete: "CASCADE",
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable("tables");
+  pgm.dropTable("Boards");
+  pgm.dropTable("users");
+};
