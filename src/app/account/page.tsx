9a9da312ed2dfeb55e62945411a68d60b181eaad
Víctor Orozco <veom104@gmail.com>
2024-03-18 20:21:26 -0700

    [ADD] add page Account

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,47 +1,123 @@
+"use client";
+import { useEffect, useState } from "react";
 import { Header } from "../lib/component/header";
 import { inter } from "../lib/ui/fonts";
+import axios from "axios";
+import { UseVerifyUser, token, url } from "@/utils/utils";
 
 export default function Account() {
-  /* // todo:
-    hacer que cada campo aparesca como placeholder el dato actual del usuario
-    darle la funcion al boton update para que haga el post y actualice los datos
-    darle la funcion a delee para que haga eliminacion de la cuenta
-*/
+  useEffect(() => {
+    UseVerifyUser();
+    fetchData();
+  }, []);
+
+  const [data, setData] = useState<any>(null);
+
+  const [formData, setFormData] = useState({
+    username: "",
+    name: "",
+    email: "",
+    password: "",
+  });
+  
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    const nextFormData = { ...formData, [name]: value };
+    setFormData(nextFormData);
+  }
+  console.log(formData);
+  console.log(data);
+
+  const fetchData = async () => {
+    try {
+      const token = sessionStorage.getItem("token");
+      const responseCards = await axios.get(url + `/account`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+      setData(responseCards.data.data);
+      console.log(responseCards.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchUpdate = async () => {
+    try {
+      const responseCards = await axios.patch(
+        url + `/account/update`,
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      setData(responseCards.data.data);
+      console.log(responseCards);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <>
-      <body className="container">
-        <Header />
-        <div className="content h-full">
-          <h1 className={`${inter.className} text-2xl font-bold`}>
-            My Account
-          </h1>
-          <div className="w-80 m-auto grid gap-4 ">
-            {/* form */}
-            <form action="" method="post" className="grid gap-4 ">
-              <div className="form_input">
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" name="username" required />
-              </div>
-              <div className="form_input">
-                <label htmlFor="name">Name</label>
-                <input type="name" id="username" name="username" required />
-              </div>
-              <div className="form_input">
-                <label htmlFor="email">Email</label>
-                <input type="email" id="username" name="username" required />
-              </div>
-              <div className="form_input">
-                <label htmlFor="password">Password</label>
-                <input type="password" id="username" name="username" required />
-              </div>
-              <button className="btnL">Update</button>
-            </form>
-            {/* delete */}
-            <button className="btnL">Delete my account</button>
-          </div>
+    <div className="container">
+      <Header />
+      <div className="content h-full">
+        <h1 className={`${inter.className} text-2xl font-bold`}>My Account</h1>
+        <div className="w-80 m-auto grid gap-4 ">
+          {/* form */}
+          <form className="grid gap-4 ">
+            <div className="form_input">
+              <label htmlFor="username">Username</label>
+              <input
+                disabled
+                type="text"
+                placeholder={data.username || ""}
+                id="username"
+                name="username"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form_input">
+              <label htmlFor="name">Name</label>
+              <input
+                type="name"
+                id="name"
+                name="name"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form_input">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                onChange={handleChange}
+                name="email"
+              />
+            </div>
+            <div className="form_input">
+              <label htmlFor="password">Password</label>
+              <input
+                placeholder="password"
+                type="password"
+                id="password"
+                onChange={handleChange}
+                name="password"
+              />
+            </div>
+            <button className="btnL" type="button" onClick={fetchUpdate}>
+              Update
+            </button>
+          </form>
+          <button className="btnL">Delete my account</button>
         </div>
-      </body>
-    </>
+      </div>
+    </div>
   );
 }
