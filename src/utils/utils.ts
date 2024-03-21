1171e5feda193177543c40439d4702559f6b9213
Víctor Orozco <veom104@gmail.com>
2024-03-21 12:58:42 -0700

    delete functions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,39 +1,30 @@
 import axios, { Method } from "axios";
-import { ReadonlyURLSearchParams } from "next/navigation";
-import { useRouter } from 'next/navigation'
+import {  ReadonlyURLSearchParams,  redirect } from "next/navigation";
 
 
 export const url = "http://localhost:5500";
+export const token: string | null = sessionStorage.getItem("token");
 
 export async function UseVerifyUser() {
-  const router = useRouter()
-
-  // Obtén el token del almacenamiento de sesión
-  const token = sessionStorage.getItem('token');
 
   if (!token) {
-    // Si no hay token, redirige al usuario a la página de inicio de sesión
-    router.push('/login');
-    return;
-  }
+    return redirect('/login');
+  } else {
 
-  try {
-    // Haz una solicitud al backend para verificar el token
-    const response = await axios.post('/api/verify-token', {}, {
+    const response = await axios.get(url +'/account',  {
       headers: {
         'Authorization': `Bearer ${token}`
       }
     });
-
-    if (!response.data.success) {
+    
+    if (!response.data.ok) {
       // Si la verificación del token falla, redirige al usuario a la página de inicio de sesión
-      router.push('/login');
+      console.log("Unauthorized");
+      redirect('/login');
     }
-  } catch (error) {
-    console.error('Error verifying token:', error);
-    router.push('/login');
   }
 }
+
 
 
 
@@ -53,7 +44,6 @@ export const InitialtableData = {
 };
 
 // export const token: string | null = sessionStorage.getItem("token");
-export const token: string | null = sessionStorage.getItem("token");
 
 // ----------tables-----------------
 // edit table
@@ -87,40 +77,3 @@ export function getParams(searchParams: ReadonlyURLSearchParams) {
   setFormData(nextFormData);
 }
 */
-
-
-
-function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-  const { name, value } = event.target;
-  const nextFormData = { ...formData, [name]: value };
-  setFormData(nextFormData);
-}
-// console.log(formData);
-const serverUrl: string = "/login";
-async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-  event.preventDefault();
-  try {
-    //!
-    const response = await axios.post(url + serverUrl, formData);
-    //*
-    const response2 = response.data.data;
-    const resMessge = response2.message;
-    const token = response.data.data.token;
-    /*
-    // console.log(response);
-    // console.log(token);
-    // console.log(userName);
-    // console.log(resMessge);
-    // console.log(resData);
-    // console.log(response2);
-    // console.log(responseOk);
-    // sessionStorage.setItem("token", token);
-    */
-    sessionStorage.setItem("token", token);
-
-    setResponse2(response2);
-    setResponseMessage(resMessge);
-  } catch (error) {
-    console.error("Error al realizar la solicitud:", error);
-  }
-}
